a3f758832e45ae25555a2274640f3e51bee03bdc
Elias Farhan <elias.farhan@gmail.com>
2025-07-19 22:51:12 +0200

    Remove direct add_subdirectory for KTX-Software and build separately

---
+++ external/build_externals.py
@@ -0,0 +1,30 @@
+import subprocess
+import os
+import sys
+
+
+path = '.'
+def build_wasm3():
+    source_dir = "{}/wasm3".format(path)
+    debug_dir = "{}/build-dbg".format(source_dir)
+    release_dir = "{}/build-rel".format(source_dir)
+
+def build_ktx():
+    print("Building KTX-Software")
+    source_dir = "{}/KTX-Software".format(path)
+    debug_dir = "{}/build-dbg".format(source_dir)
+    release_dir = "{}/build-rel".format(source_dir)
+    os.makedirs(debug_dir, exist_ok=True)
+    os.system('cmake -S {} -B {} -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF'.format(source_dir, debug_dir))
+    os.system('cmake --build {} --target ktx --config Debug'.format(debug_dir))
+    os.system('cmake --build {} --target ktx_read --config Debug'.format(debug_dir))
+    os.makedirs(release_dir, exist_ok=True)
+    os.system('cmake -S {} -B {} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF'.format(source_dir, release_dir))
+    os.system('cmake --build {} --target ktx --config Release'.format(release_dir))
+    os.system('cmake --build {} --target ktx_read --config Release'.format(release_dir))
+
+if __name__ == "__main__":
+    if len(sys.argv) > 1:
+        path = sys.argv[1]
+    build_ktx()
+    build_wasm3()
---
+++ main/test/ktx_main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by unite on 19.07.2025.
+//
+
+#include <ktx.h>
+
+int main() {
+
+}
