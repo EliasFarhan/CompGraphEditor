f8e5aafc918f47974a2297e214feded9abf8fa35
Elias Farhan <elias.farhan@gmail.com>
2025-07-29 15:15:34 +0200

    Move assimp outside of the vcpkg.json

--- external/build_externals.py
+++ external/build_externals.py
@@ -33,11 +33,24 @@ def build_ktx():
     os.system('cmake --build {} --target ktx --config Release --parallel'.format(release_dir))
     os.system('cmake --build {} --target ktx_read --config Release --parallel'.format(release_dir))
 
+def build_assimp():
+    print("Building Assimp")
+    source_dir = "{}/assimp".format(path)
+    debug_dir = "{}/build-dbg".format(source_dir)
+    release_dir = "{}/build-rel".format(source_dir)
+    os.makedirs(debug_dir, exist_ok=True)
+    os.system('''cmake -S {} -B {} -DCMAKE_BUILD_TYPE=Debug -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DASSIMP_BUILD_ASSIMP_VIEW=OFF -DASSIMP_BUILD_DRACO=ON'''.format(source_dir, debug_dir))
+    os.system('cmake --build {} --target assimp --config Debug --parallel'.format(debug_dir))
+    os.makedirs(release_dir, exist_ok=True)
+    os.system('''cmake -S {} -B {} -DCMAKE_BUILD_TYPE=Release -DBUILD_SHARED_LIBS=OFF -DASSIMP_BUILD_TESTS=OFF -DASSIMP_INSTALL=OFF -DASSIMP_BUILD_ASSIMP_VIEW=OFF -DASSIMP_BUILD_DRACO=ON'''.format(source_dir, release_dir))
+    os.system('cmake --build {} --target assimp --config Release --parallel'.format(release_dir))
+
 def build_all(from_path = "."):
     global path
     path = from_path
     build_ktx()
     build_wasm3()
+    build_assimp()
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
