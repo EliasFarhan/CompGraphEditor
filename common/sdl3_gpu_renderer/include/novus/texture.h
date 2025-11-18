f445bc45f28cbbfe3304d291b0ca9dfc815877bb
Elias Farhan <elias.farhan@gmail.com>
2025-11-18 14:12:54 +0100

    Adding sampler generation function

--- common/sdl3_gpu_renderer/include/novus/texture.h
+++ common/sdl3_gpu_renderer/include/novus/texture.h
@@ -9,12 +9,51 @@
 #include <SDL3/SDL_gpu.h>
 #include "generated/renderer_generated.h"
 
+namespace novus
+{
+
+struct SamplerInfo
+{
+    float mipLodBias;
+    float maxAnisotropy;
+    float minLod;
+    float maxLod;
+    union Bools
+    {
+        struct CompressedSamplerInfo
+        {
+            int32_t compareOp : 5;
+            int32_t addressModeU : 2;
+            int32_t addressModeV : 2;
+            int32_t addressModeW : 2;
+            int32_t minFilter : 1;
+            int32_t magFilter : 1;
+            int32_t mipmapMode : 1;
+            int32_t enableAnisotropy : 1;
+            int32_t enableCompare : 1;
+        };
+        CompressedSamplerInfo value;
+        int32_t hash;
+    };
+    Bools bools;
+    constexpr bool operator==(const SamplerInfo& other) const
+    {
+        return bools.hash == other.bools.hash && mipLodBias == other.mipLodBias && maxAnisotropy == other.maxAnisotropy && minLod == other.minLod && maxLod == other.maxLod;
+    }
+};
+
+}
+
 struct MyHash
 {
-    std::size_t operator()(const novus::internal::SamplerInfoT& s) const noexcept
+    std::size_t operator()(const novus::SamplerInfo& s) const noexcept
     {
-
-        return 0; // or use boost::hash_combine
+        std::size_t hashValue = std::hash<float>{}(s.mipLodBias);
+        hashValue = hashValue ^(std::hash<float>{}(s.maxAnisotropy)<<1);
+        hashValue = hashValue ^(std::hash<float>{}(s.minLod)<<1);
+        hashValue = hashValue ^(std::hash<float>{}(s.maxLod)<<1);
+        hashValue = hashValue ^(std::hash<int32_t>{}(s.bools.hash)<<1);
+        return hashValue; // or use boost::hash_combine
     }
 };
 
@@ -29,11 +68,12 @@ class TextureManager : public core::TextureManager
 {
 public:
 	core::TextureId LoadTexture(const renderer::TextureT& textureInfo) override;
-    const Texture& GetTexture(core::TextureId textureId);
+    const Texture& GetTexture(core::TextureId textureId) const;
 	void Clear() override;
 private:
-    std::unordered_map<internal::SamplerInfoT, SDL_GPUSampler*> sampleMap_;
-
+    SDL_GPUSampler* GenerateSampler(internal::SamplerInfoT& samplerInfo);
+    std::unordered_map<SamplerInfo, SDL_GPUSampler*, MyHash> sampleMap_;
+    std::vector<Texture> textures_;
 };
 }
 #endif //NEKO2_TEXTURE_H
--- common/sdl3_gpu_renderer/src/novus/texture.cpp
+++ common/sdl3_gpu_renderer/src/novus/texture.cpp
@@ -1,16 +1,60 @@
 #include "novus/texture.h"
 
+#include "novus/engine.h"
+
 namespace novus
 {
 core::TextureId TextureManager::LoadTexture(const renderer::TextureT& textureInfo)
 {
+
+    auto* sampler = GenerateSampler(*textureInfo.sampler);
 	return {};
 }
-const Texture& TextureManager::GetTexture(core::TextureId textureId)
+const Texture& TextureManager::GetTexture(core::TextureId textureId) const
 {
+    return textures_[static_cast<int>(textureId)];
 }
 
 void TextureManager::Clear()
 {
+    
 }
+SDL_GPUSampler* TextureManager::GenerateSampler(internal::SamplerInfoT& samplerInfo)
+{
+    SamplerInfo newSamplerInfo{.mipLodBias = samplerInfo.mip_lod_bias,
+        .maxAnisotropy = samplerInfo.max_anisotropy,
+        .minLod = samplerInfo.min_lod,
+        .maxLod = samplerInfo.max_lod,
+        .bools = {.value =
+            {.compareOp = samplerInfo.compare_op, .addressModeU = samplerInfo.address_mode_u,
+            .addressModeV = samplerInfo.address_mode_v, .addressModeW = samplerInfo.address_mode_w,
+            .minFilter = samplerInfo.min_filter, .magFilter = samplerInfo.mag_filter,
+            .mipmapMode = samplerInfo.mipmap_mode, .enableAnisotropy = samplerInfo.enable_anisotropy,
+            .enableCompare = samplerInfo.enable_compare}}};
+
+    auto it = sampleMap_.find(newSamplerInfo);
+    if (it != sampleMap_.end())
+    {
+        return it->second;
+    }
+    SDL_GPUSamplerCreateInfo createInfo{.min_filter = (SDL_GPUFilter)samplerInfo.min_filter,
+        .mag_filter = (SDL_GPUFilter)samplerInfo.mag_filter,
+        .mipmap_mode = (SDL_GPUSamplerMipmapMode)samplerInfo.mipmap_mode,
+        .address_mode_u = (SDL_GPUSamplerAddressMode)samplerInfo.address_mode_u,
+        .address_mode_v = (SDL_GPUSamplerAddressMode)samplerInfo.address_mode_v,
+        .address_mode_w = (SDL_GPUSamplerAddressMode)samplerInfo.address_mode_w,
+        .mip_lod_bias = samplerInfo.mip_lod_bias, .max_anisotropy = samplerInfo.max_anisotropy,
+        .compare_op = (SDL_GPUCompareOp)samplerInfo.compare_op,
+        .min_lod = samplerInfo.min_lod,
+        .max_lod = samplerInfo.max_lod,
+        .enable_anisotropy = samplerInfo.enable_anisotropy,
+        .enable_compare = samplerInfo.enable_compare};
+    auto* sampler = SDL_CreateGPUSampler(GetDevice(), &createInfo);
+    if (sampler ==nullptr)
+    {
+        throw std::runtime_error("SDL_CreateGPUSampler failed");
+    }
+    sampleMap_.insert(std::make_pair(newSamplerInfo, sampler));
+    return sampler;
 }
+} // namespace novus
